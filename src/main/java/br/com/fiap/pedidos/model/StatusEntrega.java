2a68b05867d5f58f8fd30bf285364cec525c54f9
gabriel <gabriellrocha.contato@gmail.com>
2026-07-26 15:37:48 -0300

    alterações classes

--- src/main/java/br/com/fiap/pedidos/model/StatusEntrega.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.pedidos.model;
-
-public enum StatusEntrega {
-    EM_SEPARACAO, EM_TRANSPORTE, ENTREGUE
-}
--- src/main/java/br/com/fiap/pedidos/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.pedidos.repository;
-
-import br.com.fiap.pedidos.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-}
