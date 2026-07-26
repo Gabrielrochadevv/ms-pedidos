b6e086caf967b9c78caa15563024d11b83bca12a
gabriel <gabriellrocha.contato@gmail.com>
2026-07-26 15:18:37 -0300

    implementa criação, atualizacao e exclusão de pedidos

---
+++ src/main/java/br/com/fiap/pedidos/controller/OrderController.java
@@ -0,0 +1,45 @@
+package br.com.fiap.pedidos.controller;
+
+import br.com.fiap.pedidos.dto.OrderRequestDto;
+import br.com.fiap.pedidos.dto.OrderResponseDto;
+import br.com.fiap.pedidos.service.OrderService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @Autowired
+    private Environment environment;
+
+
+    @PostMapping
+    public OrderResponseDto create(@RequestBody @Valid OrderRequestDto orderDto) {
+        return service.create(orderDto);
+    }
+
+    @PutMapping
+    public OrderResponseDto update(@RequestBody OrderRequestDto orderDto) {
+        return service.update(orderDto);
+    }
+
+    @DeleteMapping("{orderNumber}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long orderNumber) {
+        service.delete(orderNumber);
+    }
+
+
+
+}
---
+++ src/main/java/br/com/fiap/pedidos/service/OrderService.java
@@ -0,0 +1,52 @@
+package br.com.fiap.pedidos.service;
+
+import br.com.fiap.pedidos.dto.OrderRequestDto;
+import br.com.fiap.pedidos.dto.OrderResponseDto;
+import br.com.fiap.pedidos.exception.OrderNotFoundException;
+import br.com.fiap.pedidos.http.DeliveryClient;
+import br.com.fiap.pedidos.model.Order;
+import br.com.fiap.pedidos.model.DeliveryStatus;
+import br.com.fiap.pedidos.repository.OrderRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public OrderResponseDto create(OrderRequestDto orderDto) {
+        Order order = new Order();
+        BeanUtils.copyProperties(orderDto, order);
+
+        order.setDeliveryStatus(DeliveryStatus.IN_SEPARATION);
+        Order orderCreate = orderRepository.save(order);
+
+        return new OrderResponseDto(orderCreate);
+
+    }
+
+    public OrderResponseDto update(OrderRequestDto orderDto) {
+        Order order = new Order();
+        BeanUtils.copyProperties(orderDto, order);
+        Order updatedOrder = orderRepository.save(order);
+        return new OrderResponseDto(updatedOrder);
+    }
+
+    public void delete(Long orderNumber) {
+        Optional<Order> orderOptional = orderRepository.findById(orderNumber);
+
+        if (orderOptional.isPresent()) {
+            orderRepository.delete(orderOptional.get());
+        } else {
+            throw new OrderNotFoundException(String.format("Pedido %s não existe!", orderNumber));
+        }
+        Order order = new Order();
+    }
+
+}
